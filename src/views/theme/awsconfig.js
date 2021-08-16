a7514b8aef2eff65666beff8888f6b0e9d4cc4a1
Jeon <myeongsu@3c22fbccb5a0.ant.amazon.com>
2021-08-17 03:43:10 +0900

    Cognito '

--- src/views/theme/awsconfig.js
+++ src/views/theme/awsconfig.js
@@ -2,6 +2,19 @@ export default {
   Auth: {
     region: "us-east-1",
     userPoolId: "us-east-1_8PmOLN8mA",
-    userPoolWebClientId: "48q14q979ohgjg1kmt172uab2b"
+    userPoolWebClientId: "48q14q979ohgjg1kmt172uab2b",
+    oauth: {
+        "domain": "lamaabp.auth.us-east-1.amazoncognito.com/",
+        "scope": [
+            "phone",
+            "email",
+            "openid",
+            "profile",
+            "aws.cognito.signin.user.admin"
+        ],
+        "redirectSignIn": "https://master.d1o62audtbihat.amplifyapp.com/cognitologin",
+        "redirectSignOut": "https://master.d1o62audtbihat.amplifyapp.com/cognitologin",
+        "responseType": "code"
+    },
   }
 }
