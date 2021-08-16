961c6422534e3203d7e8493113eb29eb873bbece
Jeon <myeongsu@3c22fbccb5a0.ant.amazon.com>
2021-08-17 00:03:23 +0900

    Cognito

--- src/views/theme/CognitoLogin.vue
+++ src/views/theme/CognitoLogin.vue
@@ -1,34 +1,32 @@
 <template>
   <amplify-authenticator>
-    <div>
-      My App
+    <div v-if="authState === 'signedin' && user">
+      My App, {{ user.username }} !!!
       <amplify-sign-out></amplify-sign-out>
     </div>
   </amplify-authenticator>
 </template>
 
 <script>
-import { Auth } from 'aws-amplify';
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+import awsconfig from './awsconfig';
+import Amplify from 'aws-amplify';
 import '@aws-amplify/ui-vue';
+Amplify.configure(awsconfig);
+
 export default {
   name: "CognitoLogin",
-  created () {           // 초기화 함수를 정의 한다.
-    Auth.currentAuthenticatedUser({
-        bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-    }).then(user => console.log(user))
-    .catch(err => console.log(err));
-    console.log("aaaa");
-
-    console.log(Auth.currentUserInfo())
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange(
+        (authState, authData) => { this.authState = authState; this.user = authData; })
   },
-  mounted () {
-   Auth.currentUserInfo()
-      .then((res) => {
-        console.log('Here are the current user info! =>', res);
-      })
-      .catch((err) => {
-        console.log('Current user info failed to fetch', err);
-      });
+  data() {
+    return {
+      user: undefined, authState: undefined, unsubscribeAuth: undefined
+    }
+  },
+  beforeUnmount() {
+    this.unsubscribeAuth();
   }
 }
 </script>
---
+++ src/views/theme/awsconfig.js
@@ -0,0 +1,7 @@
+export default {
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_8PmOLN8mA",
+    userPoolWebClientId: "48q14q979ohgjg1kmt172uab2b"
+  }
+}
